5486ade967fe4fae9897ca17216b41bce972ee60
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-09-02 12:40:10 +0900

    CommandInvoker作成

--- Game/Commands/ObjectCommandInvoker.cpp
+++ Game/Commands/ObjectCommandInvoker.cpp
@@ -1,1 +1,62 @@
 #include "ObjectCommandInvoker.h"
+
+void ObjectCommandInvoker::ExecuteCommandRequest(){}
+void ObjectCommandInvoker::ExecuteCommandRequest(){
+	// コマンドを積まれてなければスキップ
+	if(commandRequests_.commandQueue_.empty()){
+		return;
+	}
+
+	// 積まれたコマンドを全て実行する
+	for(auto& cmd : commandRequests_.commandQueue_){
+		cmd->Execute();
+	}
+
+	// 履歴に保存
+	// currentCommandItr_ より先が残っていたら破棄（Redoのやり直し不可）
+	commandHistory_.erase(std::next(currentCommandItr_),commandHistory_.end());
+
+	// 新しい FrameCommand を履歴に push
+	commandHistory_.push_back(std::make_unique<FrameCommand>(std::move(commandRequests_)));
+	currentCommandItr_ = std::prev(commandHistory_.end());
+
+	// 次のフレーム用に空の commandRequests_ を用意
+	commandRequests_ = FrameCommand{};
+}
+
+void ObjectCommandInvoker::UndoCommand(){
+	if(currentCommandItr_ == commandHistory_.end()){
+		return;
+	}
+
+	// 逆順に Undo
+	for(auto it = (*currentCommandItr_)->commandQueue_.rbegin();
+		it != (*currentCommandItr_)->commandQueue_.rend(); ++it){
+		(*it)->Undo();
+	}
+
+	if(currentCommandItr_ != commandHistory_.begin()){
+		--currentCommandItr_;
+	} else{
+		currentCommandItr_ = commandHistory_.end(); // すべてUndo済み
+	}
+}
+
+void ObjectCommandInvoker::RedoCommand(){
+	if(commandHistory_.empty()){
+		return;
+	}
+
+	if(currentCommandItr_ == commandHistory_.end()){
+		currentCommandItr_ = commandHistory_.begin();
+	} else if(std::next(currentCommandItr_) != commandHistory_.end()){
+		++currentCommandItr_;
+	} else{
+		return; // これ以上Redoできない
+	}
+
+	// 順番通りに Redo
+	for(auto& cmd : (*currentCommandItr_)->commandQueue_){
+		cmd->Execute(); // Redoも基本的に再実行
+	}
+}
--- Game/Commands/ObjectCommandInvoker.h
+++ Game/Commands/ObjectCommandInvoker.h
@@ -1,9 +1,50 @@
 #pragma once
 
+/// container
+#include <list>
+#include <vector>
+#include <queue>
+/// smart pointer
+#include <memory>
 
+/// command
+#include "IObjectCommand.h"
 
+/// <summary>
+/// コマンドを実行するクラス
+/// 基本は reqestQueue にコマンドを積んでおいて、ExecuteCommandRequest() で実行する
+/// </summary>
 class ObjectCommandInvoker{
+	friend class UndoCommand;
+	friend class RedoCommand;
 public:
+	static ObjectCommandInvoker& getInstance(){
+		static ObjectCommandInvoker instance;
+		return instance;
+	}
+
+public:
+	/// <summary>
+	/// 積まれた コマンドを実行する
+	/// </summary>
+	void ExecuteCommandRequest();
+
+	void UndoCommand();
+	void RedoCommand();
+
+private:
+	ObjectCommandInvoker() = default;
+	ObjectCommandInvoker(const ObjectCommandInvoker&) = delete;
+	ObjectCommandInvoker& operator=(const ObjectCommandInvoker&) = delete;
+	~ObjectCommandInvoker() = default;
+
+private:
+	struct FrameCommand{
+		std::vector<std::unique_ptr<IObjectCommand>> commandQueue_;
+	};
+
+	FrameCommand commandRequests_;
+	std::list<std::unique_ptr<FrameCommand>> commandHistory_;
+	std::list<std::unique_ptr<FrameCommand>>::iterator currentCommandItr_ = commandHistory_.end();
 
 };
-
