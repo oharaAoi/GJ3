747ae3ac176457ae02a33a90ffea3a17f382c65b
Onuma0128 <147121758+Onuma0128@users.noreply.github.com>
2025-09-02 18:16:27 +0900

    ゴーストブロックの周りの判定を追加

--- Game/Manager/MapCollisionSystem.cpp
+++ Game/Manager/MapCollisionSystem.cpp
@@ -6,21 +6,20 @@ void MapCollisionSystem::Init(StageRegistry* stageRegistry)
 
 }
 
-bool MapCollisionSystem::IsMovable(const Vector2Int& direction, IBlock* player)
+bool MapCollisionSystem::IsMovable(const Vector2Int& direction, const Vector2Int& playerIndex)
 {
 	// プレイヤーと進む方向のblockTypeとindexを取得
 	const auto& data = stageRegistry_->GetStageData();
 
 	// プレイヤーブロック
-	IBlock* p = player;
-	Vector2Int index = p->GetIndex() + direction;
+	Vector2Int index = playerIndex + direction;
 
 	// 進む方向1マス目ブロック
 	IBlock* one = data[index.x][index.y].get();
 	if (one->GetType() == BlockType::Wall) {			// 壁だから進めない
 		return false;								
 	} else if (one->GetType() == BlockType::None) {		// 何もないなら進める
-		ChengeStage(direction, player);
+		ChengeStage(direction, playerIndex);
 		return true; 
 	}
 
@@ -30,7 +29,7 @@ bool MapCollisionSystem::IsMovable(const Vector2Int& direction, IBlock* player)
 	// 1マス目が動かせるブロックで2マス目がないなら早期return
 	if (one->GetType() == BlockType::NormalBlock || one->GetType() == BlockType::GhostBlock) {
 		if (two->GetType() == BlockType::None) {
-			ChengeStage(direction, player, true);
+			ChengeStage(direction, playerIndex, true);
 			return true;
 		}
 	}
@@ -38,10 +37,41 @@ bool MapCollisionSystem::IsMovable(const Vector2Int& direction, IBlock* player)
 	return false;
 }
 
-void MapCollisionSystem::ChengeStage(const Vector2Int& direction, IBlock* player, bool isSetBlock)
+void MapCollisionSystem::UpdateSpanGhost()
+{
+	// ステージデータからゴーストブロックを取得
+	const auto& data = stageRegistry_->GetStageData();
+	pairIndex_.clear();
+	for (const auto& row : data) {
+		for (const auto& col : row) {
+			// ゴーストブロックじゃ無ければコンティニュー
+			if (col->GetType() != BlockType::GhostBlock) { continue; }
+			// ゴーストブロックだったら
+			Vector2Int index = col->GetIndex();
+			std::list<Vector2Int> indexs;
+			indexs.push_back(index + Vector2Int{ 2,0 });
+			indexs.push_back(index + Vector2Int{ -2,0 });
+			indexs.push_back(index + Vector2Int{ 0,-2 });
+			indexs.push_back(index + Vector2Int{ 0,2 });
+			for (auto& _index : indexs) {
+				if (CheckGhostBlock(_index)) {
+					pairIndex_.push_back(
+						Vector2Int{
+							index.x * static_cast<int>(row.size()) + index.y,
+							_index.x * static_cast<int>(row.size()) + _index.y,
+						}
+					);
+				}
+			}
+		}
+	}
+
+}
+
+void MapCollisionSystem::ChengeStage(const Vector2Int& direction, const Vector2Int& playerIndex, bool isSetBlock)
 {
 	// プレイヤーの元居たIndex番号からうめていく
-	Vector2Int index = player->GetIndex();
+	Vector2Int index = playerIndex;
 	const auto& data = stageRegistry_->GetStageData();
 
 	data[index.x][index.y]->SetType(BlockType::None);
@@ -52,4 +82,24 @@ void MapCollisionSystem::ChengeStage(const Vector2Int& direction, IBlock* player
 		index += direction;
 		data[index.x][index.y]->SetType(type);
 	}
+}
+
+bool MapCollisionSystem::CheckGhostBlock(const Vector2Int& index)
+{
+	const auto& data = stageRegistry_->GetStageData();
+	size_t row = data.size();
+	size_t col = data[0].size();
+	// 範囲外では無いか検出
+	if (index.x >= row || index.y >= col) { return false; }
+	// ゴーストブロックか判定
+	if (data[index.x][index.y]->GetType() == BlockType::GhostBlock) { 
+		for (auto& pair : pairIndex_) {
+			if ((pair.x == index.x && pair.y == index.y) ||
+				(pair.y == index.x && pair.x == index.y)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	return false;
 }
