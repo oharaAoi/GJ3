2c1604b7cca21cde32869ce4d26c94af95a43776
OriharaIssei <k023g0048@g.neec.ac.jp>
2025-09-06 14:34:45 +0900

    向きの変更を検知

--- Game/Actor/Player/Player.h
+++ Game/Actor/Player/Player.h
@@ -26,16 +26,24 @@ public:
 	MapCollisionSystem* GetMapCollision(){ return mapCollision_; }
 	void SetMapCollision(MapCollisionSystem* collision){ mapCollision_ = collision; }
 
+	enum class SpriteDirection{
+		LEFT,
+		RIGHT,
+	};
+
 protected:
 	std::unique_ptr<PlayerInputHandler> inputHandler_; // 入力ハンドラ
 private:
 	const float animationTime_ = 0.2f; // アニメーションの時間
 	float animationTimer_ = 0.0f; // アニメーションのタイマー
 	bool isAnimation_ = false;
+	SpriteDirection spriteDirection_ = SpriteDirection::RIGHT; // プレイヤーの向き
 	Vector2 ConvertIndexToPosition(const Vector2Int& _index);	// 座標計算処理
-
 	MapCollisionSystem* mapCollision_ = nullptr;
 public:
 	const bool GetIsAnimation() const{ return isAnimation_; }
 	void SetIsAnimation(const bool flag){ isAnimation_ = flag; }
+
+	const SpriteDirection& GetSpriteDirection() const{ return spriteDirection_; }
+	void SetSpriteDirection(const SpriteDirection& direction){ spriteDirection_ = direction; }
 };
--- Game/Commands/Player/PlayerMoveCommand.cpp
+++ Game/Commands/Player/PlayerMoveCommand.cpp
@@ -7,29 +7,78 @@ constexpr Vector2Int MOVE_VECTOR_DOWN = {0,1};
 
 void PlayerLeftMoveCommand::Execute(){
 	host_->SetIndex(host_->GetIndex() + MOVE_VECTOR_LEFT);
+
+
+	// 向きを左に変える
+	preDirection_ = host_->GetSpriteDirection();
+	if(preDirection_ != Player::SpriteDirection::LEFT){
+		host_->SetSpriteDirection(Player::SpriteDirection::LEFT);
+	}
 }
 
 void PlayerLeftMoveCommand::Undo(){
 	host_->SetIndex(host_->GetIndex() - MOVE_VECTOR_LEFT);
+
+	// 向きを元に戻す
+	Player::SpriteDirection currentDirection = host_->GetSpriteDirection();
+	if(preDirection_ != currentDirection){
+		host_->SetSpriteDirection(Player::SpriteDirection::LEFT);
+	}
 }
 
 void PlayerRightMoveCommand::Execute(){
 	host_->SetIndex(host_->GetIndex() + MOVE_VECTOR_RIGHT);
+
+	// 向きを右に変える
+	preDirection_ = host_->GetSpriteDirection();
+	if(preDirection_ != Player::SpriteDirection::RIGHT){
+		host_->SetSpriteDirection(Player::SpriteDirection::RIGHT);
+	}
 }
 void PlayerRightMoveCommand::Undo(){
 	host_->SetIndex(host_->GetIndex() - MOVE_VECTOR_RIGHT);
+
+	// 向きを元に戻す
+	Player::SpriteDirection currentDirection = host_->GetSpriteDirection();
+	if(preDirection_ != currentDirection){
+		host_->SetSpriteDirection(Player::SpriteDirection::RIGHT);
+	}
 }
 
 void PlayerUpMoveCommand::Execute(){
 	host_->SetIndex(host_->GetIndex() + MOVE_VECTOR_UP);
+
+	// 向きを上に変える
+	preDirection_ = host_->GetSpriteDirection();
+	if(preDirection_ != Player::SpriteDirection::RIGHT){
+		host_->SetSpriteDirection(Player::SpriteDirection::RIGHT);
+	}
 }
 void PlayerUpMoveCommand::Undo(){
 	host_->SetIndex(host_->GetIndex() - MOVE_VECTOR_UP);
+
+	// 向きを元に戻す
+	Player::SpriteDirection currentDirection = host_->GetSpriteDirection();
+	if(preDirection_ != currentDirection){
+		host_->SetSpriteDirection(Player::SpriteDirection::RIGHT);
+	}
 }
 
 void PlayerDownMoveCommand::Execute(){
 	host_->SetIndex(host_->GetIndex() + MOVE_VECTOR_DOWN);
+
+	// 向きを下に変える
+	preDirection_ = host_->GetSpriteDirection();
+	if(preDirection_ != Player::SpriteDirection::RIGHT){
+		host_->SetSpriteDirection(Player::SpriteDirection::RIGHT);
+	}
 }
 void PlayerDownMoveCommand::Undo(){
 	host_->SetIndex(host_->GetIndex() - MOVE_VECTOR_DOWN);
+
+	// 向きを元に戻す
+	Player::SpriteDirection currentDirection = host_->GetSpriteDirection();
+	if(preDirection_ != currentDirection){
+		host_->SetSpriteDirection(Player::SpriteDirection::RIGHT);
+	}
 }
